106da0c0cd7a7c6fae7c7e1cab93d8b51ebb7b9a
Ken-Miura <ken.miura1102@gmail.com>
2017-04-02 02:39:49 +0900

    リファクタリング

--- ch01/ex04/main.go
+++ ch01/ex04/main.go
@@ -13,7 +13,7 @@ func main() {
 	lineToFiles := make(map[string][]string)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, lineToFiles)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -21,40 +21,26 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, counts, lineToFiles)
 			f.Close()
-
-			f2, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup: %v\n", err)
-				continue
-			}
-			findLines(f2, lineToFiles)
-			f2.Close()
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
-			fmt.Println(line + ": found in " + strings.Join(lineToFiles[line], ", "))
+			fmt.Printf("%d\t%s", n, line)
+			fmt.Println("\tfound in " + strings.Join(lineToFiles[line], ", "))
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int, lineToFiles map[string][]string) {
 	input := bufio.NewScanner(f)
+Counts:
 	for input.Scan() {
 		counts[input.Text()]++
-	}
-	// NOTE: ignoring potential errors from input.Err()
-}
-
-func findLines(f *os.File, lineToFiles map[string][]string) {
-	input := bufio.NewScanner(f)
-	for input.Scan() {
 		for _, file := range lineToFiles[input.Text()] {
 			if file == f.Name() {
-				return
+				continue Counts
 			}
 		}
 		lineToFiles[input.Text()] = append(lineToFiles[input.Text()], f.Name())
