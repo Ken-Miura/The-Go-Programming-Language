8c2b20fb0333cf1b877468aded78f02406bdfb71
Ken-Miura <ken.miura1102@gmail.com>
2017-04-02 19:07:18 +0900

    練習問題追加

---
+++ ch01/ex09/main.go
@@ -0,0 +1,32 @@
+// Copyright 2017 Ken Mirua
+// Fetch prints the content found at each specified URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !(strings.HasPrefix(url, "http://") || strings.HasPrefix(url, "https://")) {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		bytesCopied, err := io.Copy(os.Stdout, resp.Body)
+		fmt.Println()
+		fmt.Println("status code: " + resp.Status)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v at %d byte(s) copied\n", url, err, bytesCopied)
+			os.Exit(1)
+		}
+	}
+}
