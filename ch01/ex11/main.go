08a0517fcf1e8a379cb6bdedd2a41424d696b4bb
Ken-Miura <ken.miura1102@gmail.com>
2017-04-09 01:20:31 +0900

    練習問題追加 引数リストが長すぎるとコマンドラインに渡せる文字列数の制限（プログラム名含め8192文字まで）に引っかかるのでファイルからの読み込みで実装

---
+++ ch01/ex11/main.go
@@ -0,0 +1,51 @@
+// Copyright 2017 Ken Mirua
+// Fetchall fetches URLs in parallel and reports their times and sizes.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, file := range os.Args[1:] {
+		f, err := os.Open(file)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup: %v\n", err)
+			continue
+		}
+		input := bufio.NewScanner(f)
+		for input.Scan() {
+			go fetch(input.Text(), ch) // start a goroutine
+		}
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
