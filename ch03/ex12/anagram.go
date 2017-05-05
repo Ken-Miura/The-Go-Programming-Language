8ab54717db67123db98ea83c44ed8d9006dbe6b1
Ken-Miura <ken.miura1102@gmail.com>
2017-05-06 01:09:38 +0900

    練習問題追加

---
+++ ch03/ex12/anagram.go
@@ -0,0 +1,36 @@
+// Copyright 2017 Ken Mirua
+package ex12
+
+import (
+	"unicode"
+	"unicode/utf8"
+)
+
+func IsAnagram(s1, s2 string) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	if utf8.RuneCountInString(s1) != utf8.RuneCountInString(s2) {
+		return false
+	}
+
+	runeCount1 := make(map[rune]int)
+	for _, r1 := range s1 {
+		r1 = unicode.ToLower(unicode.ToUpper(r1))
+		runeCount1[r1]++
+	}
+
+	runeCount2 := make(map[rune]int)
+	for _, r2 := range s2 {
+		r2 = unicode.ToLower(unicode.ToUpper(r2))
+		runeCount2[r2]++
+	}
+
+	for k1, v1 := range runeCount1 {
+		v2, ok := runeCount2[k1]
+		if !ok || v1 != v2 {
+			return false
+		}
+	}
+	return true
+}
---
+++ ch03/ex12/anagram_test.go
@@ -0,0 +1,26 @@
+// Copyright 2017 Ken Mirua
+package ex12
+
+import (
+	"testing"
+)
+
+var tests = []struct {
+	input1   string // input
+	input2   string // input
+	expected bool   //  expected
+}{
+	{"ab", "cd", false},
+	{"canoe", "ocean", true},
+	{"こんにちは", "世界", false},
+	{"かとうあい", "あとうかい", true},
+}
+
+func TestComma(t *testing.T) {
+	for _, tt := range tests {
+		actual := IsAnagram(tt.input1, tt.input2)
+		if actual != tt.expected {
+			t.Fatalf("expected is %t, but actual is %t (input1: %s, input2: %s)", tt.expected, actual, tt.input1, tt.input2)
+		}
+	}
+}
