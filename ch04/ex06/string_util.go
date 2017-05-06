cc1b2e07dbc51278fb653632dcbf4fe6cad9e3c8
Ken-Miura <ken.miura1102@gmail.com>
2017-05-07 05:54:07 +0900

    練習問題追加

---
+++ ch04/ex06/string_util.go
@@ -0,0 +1,33 @@
+// Copyright 2017 Ken Mirua
+package ex06
+
+import (
+	"unicode"
+)
+
+func CompressSpaces(s []byte) []byte {
+	runes := []rune(string(s))
+	runes = repalaceSpacesToAsciiSpase(runes)
+	for i := 0; i < len(runes)-1; {
+		if unicode.IsSpace(runes[i]) && unicode.IsSpace(runes[i+1]) {
+			runes = remove(runes, i+1)
+			continue
+		}
+		i++
+	}
+	return []byte(string(runes))
+}
+
+func repalaceSpacesToAsciiSpase(runes []rune) []rune {
+	for i := range runes {
+		if unicode.IsSpace(runes[i]) && runes[i] != ' ' {
+			runes[i] = ' '
+		}
+	}
+	return runes
+}
+
+func remove(slice []rune, i int) []rune {
+	copy(slice[i:], slice[i+1:])
+	return slice[:len(slice)-1]
+}
---
+++ ch04/ex06/string_util_test.go
@@ -0,0 +1,28 @@
+// Copyright 2017 Ken Mirua
+package ex06
+
+import "testing"
+
+var tests = []struct {
+	input    []byte
+	expected []byte
+}{
+	{[]byte("a b c"), []byte("a b c")},
+	{[]byte("a  b  c"), []byte("a b c")},
+	{[]byte("a   b   c"), []byte("a b c")},
+	{[]byte("  "), []byte(" ")},
+	{[]byte("a　b"), []byte("a b")},
+	{[]byte("a\u200A b"), []byte("a b")},
+	{[]byte("abc"), []byte("abc")},
+}
+
+func TestCompressSpaces(t *testing.T) {
+	for _, tt := range tests {
+		actual := CompressSpaces(tt.input)
+		for i := range tt.expected {
+			if actual[i] != tt.expected[i] {
+				t.Fatalf("expected is %v, but actual is %v", tt.expected, actual)
+			}
+		}
+	}
+}
