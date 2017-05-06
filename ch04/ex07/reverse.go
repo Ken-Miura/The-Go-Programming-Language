fbf0664754773900dba3270c1d11d47994ed98b9
Ken-Miura <ken.miura1102@gmail.com>
2017-05-07 06:32:57 +0900

    TODO追加

--- ch04/ex07/reverse.go
+++ ch04/ex07/reverse.go
@@ -1,6 +1,7 @@
 // Copyright 2017 Ken Mirua
 package ex07
 
+// TODO 追加のメモリ割り当てなしのバージョン
 func Reverse(s []byte) []byte {
 	runes := []rune(string(s))
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
