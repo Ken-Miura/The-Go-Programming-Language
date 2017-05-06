c576088362bcbe66fa240ed24b299ef21f0b92af
Ken-Miura <ken.miura1102@gmail.com>
2017-05-07 06:15:17 +0900

    練習問題追加（メモリ追加割り当て必要なバージョン）

---
+++ ch04/ex07/reverse.go
@@ -0,0 +1,10 @@
+// Copyright 2017 Ken Mirua
+package ex07
+
+func Reverse(s []byte) []byte {
+	runes := []rune(string(s))
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return []byte(string(runes))
+}
---
+++ ch04/ex07/reverse_test.go
@@ -0,0 +1,23 @@
+// Copyright 2017 Ken Mirua
+package ex07
+
+import "testing"
+
+var tests = []struct {
+	input    []byte
+	expected []byte
+}{
+	{[]byte("abc"), []byte("cba")},
+	{[]byte("世界"), []byte("界世")},
+}
+
+func TestReverse(t *testing.T) {
+	for _, tt := range tests {
+		actual := Reverse(tt.input)
+		for i := range tt.expected {
+			if actual[i] != tt.expected[i] {
+				t.Fatalf("expected is %v, but actual is %v", tt.expected, actual)
+			}
+		}
+	}
+}
