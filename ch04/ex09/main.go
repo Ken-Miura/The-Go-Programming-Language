ddeb59aa2f9bca52ebc81a572be95aaa6bf1fc4d
Ken-Miura <ken.miura1102@gmail.com>
2017-05-08 02:23:14 +0900

    練習問題追加

---
+++ ch04/ex09/main.go
@@ -0,0 +1,36 @@
+// Copyright 2017 Ken Mirua
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("usage: " + os.Args[0] + " 'text file in which you want to count words'")
+		fmt.Println("ex. " + os.Args[0] + " alice.txt")
+		return
+	}
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Printf("failed to open file. error: %v", err)
+		return
+	}
+	defer f.Close()
+
+	input := bufio.NewScanner(f)
+	input.Split(bufio.ScanWords)
+
+	wordfreq := make(map[string]int)
+	for input.Scan() {
+		wordfreq[input.Text()]++
+	}
+
+	fmt.Println("word : number of times that the word appears")
+	for k, v := range wordfreq {
+		fmt.Printf("%s : %d\n", k, v)
+	}
+}
