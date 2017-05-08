ad368afcace168f2a553079db7fd2a06a9319ff7
Ken-Miura <ken.miura1102@gmail.com>
2017-05-09 04:10:20 +0900

    issueの作成部分完成

--- ch04/ex11/main.go
+++ ch04/ex11/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -25,7 +26,8 @@ func main() {
 	if *operation == "list" {
 		if len(args) != 2 {
 			fmt.Println("usage: " + os.Args[0] + " [-o list] 'owner' 'repository'")
-			fmt.Println("ex1. " + os.Args[0] + " -o list Ken-Miura GitHub-API-Practice")
+			fmt.Println("ex1. " + os.Args[0] + " Ken-Miura GitHub-API-Practice")
+			fmt.Println("ex2. " + os.Args[0] + " -o list Ken-Miura GitHub-API-Practice")
 			return
 		}
 		resp, err := http.Get(gitHubAPIRoot + "/" + args[0] + "/" + args[1] + "/issues")
@@ -36,7 +38,7 @@ func main() {
 		defer resp.Body.Close()
 
 		if resp.StatusCode != http.StatusOK {
-			fmt.Println(resp.StatusCode)
+			fmt.Printf("failed to request. status code: %v\n", resp.StatusCode)
 			return
 		}
 
@@ -49,6 +51,39 @@ func main() {
 		for _, item := range issues {
 			fmt.Printf("No.%d state: %s title: %.55s\n", item.Number, item.State, item.Title)
 		}
+	} else if *operation == "create" {
+		if len(args) != 4 {
+			fmt.Println("usage: " + os.Args[0] + " -o create 'owner' 'repository' 'access token' 'title'")
+			fmt.Println("ex. " + os.Args[0] + " -o create Ken-Miura GitHub-API-Practice xxxxxxxxxx issue1")
+			return
+		}
+
+		req, err := http.NewRequest("POST", gitHubAPIRoot+"/"+args[0]+"/"+args[1]+"/issues", strings.NewReader(`{"title":"`+args[3]+`"}`))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		req.Header.Set("Authorization", "token "+args[2])
+		client := new(http.Client)
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != 201 {
+			var message struct{ Message string }
+			if err := json.NewDecoder(resp.Body).Decode(&message); err != nil {
+				fmt.Printf("failed to decode response body as json. error: %v", err)
+			}
+			fmt.Printf("failed to request. status code: %v. reason: %v", resp.StatusCode, message)
+			return
+		}
+	} else if *operation == "edit" {
+		// TODO
+	} else {
+		panic("This line must not be reached.\n")
 	}
 }
 
