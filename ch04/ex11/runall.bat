bee0f7958dd272c568bca935452b72bc6eda914b
Ken-Miura <ken.miura1102@gmail.com>
2017-05-09 04:37:13 +0900

    バッチ追加

---
+++ ch04/ex11/runall.bat
@@ -0,0 +1,4 @@
+go run main.go -o list Ken-Miura GitHub-API-Practice
+go run main.go -o create Ken-Miura GitHub-API-Practice xxxxxxx issue
+go run main.go -o edit Ken-Miura GitHub-API-Practice xxxxxxx 2 ISSUE
+go run main.go -o close Ken-Miura GitHub-API-Practice xxxxxxx 2
