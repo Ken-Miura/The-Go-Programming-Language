2f01a18f2fc06dcf0ab0a4bf9092bc7a9c263a9f
Ken-Miura <ken.miura1102@gmail.com>
2017-05-12 00:10:44 +0900

    リファクタリング

--- ch04/ex11/main.go
+++ ch04/ex11/main.go
@@ -18,126 +18,46 @@ var operation = flag.String("o", "list", "issue operation for the repository: li
 // TODO エディタ連携 エディタを起動した後にそのエディタに入力された文字列はどうやって取得すればよい？？？
 func main() {
 	flag.Parse()
-	args := flag.Args()
 	if !(*operation == "list" || *operation == "create" || *operation == "edit" || *operation == "close") {
 		fmt.Println("Operation must be list, create, edit or close.")
 		return
 	}
 
+	programName := os.Args[0]
+	args := flag.Args()
 	if *operation == "list" {
 		if len(args) != 2 {
-			fmt.Println("usage: " + os.Args[0] + " [-o list] 'owner' 'repository'")
-			fmt.Println("ex1. " + os.Args[0] + " Ken-Miura GitHub-API-Practice")
-			fmt.Println("ex2. " + os.Args[0] + " -o list Ken-Miura GitHub-API-Practice")
+			fmt.Println("usage: " + programName + " [-o list] 'owner' 'repository'")
+			fmt.Println("ex1. " + programName + " Ken-Miura GitHub-API-Practice")
+			fmt.Println("ex2. " + programName + " -o list Ken-Miura GitHub-API-Practice")
 			return
 		}
-		resp, err := http.Get(gitHubAPIRoot + "/" + args[0] + "/" + args[1] + "/issues")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			fmt.Printf("failed to request. status code: %v\n", resp.StatusCode)
-			return
-		}
-
-		var issues []Issue
-		if err := json.NewDecoder(resp.Body).Decode(&issues); err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		for _, item := range issues {
-			fmt.Printf("No.%d state: %s title: %.55s\n", item.Number, item.State, item.Title)
-		}
+		listIssues(args[0], args[1])
+		return
 	} else if *operation == "create" {
 		if len(args) != 4 {
-			fmt.Println("usage: " + os.Args[0] + " -o create 'owner' 'repository' 'access token' 'title'")
-			fmt.Println("ex. " + os.Args[0] + " -o create Ken-Miura GitHub-API-Practice xxxxxxxxxx issue1")
+			fmt.Println("usage: " + programName + " -o create 'owner' 'repository' 'access token' 'title'")
+			fmt.Println("ex. " + programName + " -o create Ken-Miura GitHub-API-Practice xxxxxxxxxx issue1")
 			return
 		}
-
-		req, err := http.NewRequest("POST", gitHubAPIRoot+"/"+args[0]+"/"+args[1]+"/issues", strings.NewReader(`{"title":"`+args[3]+`"}`))
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		req.Header.Set("Authorization", "token "+args[2])
-		client := new(http.Client)
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != 201 {
-			var message struct{ Message string }
-			if err := json.NewDecoder(resp.Body).Decode(&message); err != nil {
-				fmt.Printf("failed to decode response body as json. error: %v", err)
-			}
-			fmt.Printf("failed to request. status code: %v. reason: %v", resp.StatusCode, message)
-			return
-		}
+		createIssue(args[0], args[1], args[2], args[3])
+		return
 	} else if *operation == "edit" {
 		if len(args) != 5 {
-			fmt.Println("usage: " + os.Args[0] + " -o edit 'owner' 'repository' 'access token' 'issue No.' 'new title'")
-			fmt.Println("ex. " + os.Args[0] + " -o edit Ken-Miura GitHub-API-Practice xxxxxxxxxx 2 ISSUE")
+			fmt.Println("usage: " + programName + " -o edit 'owner' 'repository' 'access token' 'issue No.' 'new title'")
+			fmt.Println("ex. " + programName + " -o edit Ken-Miura GitHub-API-Practice xxxxxxxxxx 2 ISSUE")
 			return
 		}
-
-		req, err := http.NewRequest("PATCH", gitHubAPIRoot+"/"+args[0]+"/"+args[1]+"/issues/"+args[3], strings.NewReader(`{"title":"`+args[4]+`"}`))
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		req.Header.Set("Authorization", "token "+args[2])
-		client := new(http.Client)
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			var message struct{ Message string }
-			if err := json.NewDecoder(resp.Body).Decode(&message); err != nil {
-				fmt.Printf("failed to decode response body as json. error: %v", err)
-			}
-			fmt.Printf("failed to request. status code: %v. reason: %v", resp.StatusCode, message)
-			return
-		}
+		editIssue(args[0], args[1], args[2], args[3], args[4])
+		return
 	} else if *operation == "close" {
 		if len(args) != 4 {
-			fmt.Println("usage: " + os.Args[0] + " -o close 'owner' 'repository' 'access token' 'issue No.'")
-			fmt.Println("ex. " + os.Args[0] + " -o close Ken-Miura GitHub-API-Practice xxxxxxxxxx 1")
+			fmt.Println("usage: " + programName + " -o close 'owner' 'repository' 'access token' 'issue No.'")
+			fmt.Println("ex. " + programName + " -o close Ken-Miura GitHub-API-Practice xxxxxxxxxx 1")
 			return
 		}
-		req, err := http.NewRequest("PATCH", gitHubAPIRoot+"/"+args[0]+"/"+args[1]+"/issues/"+args[3], strings.NewReader(`{"state":"closed"}`))
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		req.Header.Set("Authorization", "token "+args[2])
-		client := new(http.Client)
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			var message struct{ Message string }
-			if err := json.NewDecoder(resp.Body).Decode(&message); err != nil {
-				fmt.Printf("failed to decode response body as json. error: %v", err)
-			}
-			fmt.Printf("failed to request. status code: %v. reason: %v", resp.StatusCode, message)
-			return
-		}
+		closeIssue(args[0], args[1], args[2], args[3])
+		return
 	} else {
 		panic("This line must not be reached.\n")
 	}
@@ -156,4 +76,103 @@ type Issue struct {
 type User struct {
 	Login   string
 	HTMLURL string `json:"html_url"`
+}
+
+func listIssues(owner, repository string) {
+	resp, err := http.Get(gitHubAPIRoot + "/" + owner + "/" + repository + "/issues")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("failed to request. status code: %v\n", resp.StatusCode)
+		return
+	}
+
+	var issues []Issue
+	if err := json.NewDecoder(resp.Body).Decode(&issues); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, item := range issues {
+		fmt.Printf("No.%d state: %s title: %.55s\n", item.Number, item.State, item.Title)
+	}
+}
+
+func createIssue(owner, repository, accessToken, title string) {
+	req, err := http.NewRequest("POST", gitHubAPIRoot+"/"+owner+"/"+repository+"/issues", strings.NewReader(`{"title":"`+title+`"}`))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Set("Authorization", "token "+accessToken)
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 201 {
+		var message struct{ Message string }
+		if err := json.NewDecoder(resp.Body).Decode(&message); err != nil {
+			fmt.Printf("failed to decode response body as json. error: %v", err)
+		}
+		fmt.Printf("failed to request. status code: %v. reason: %v", resp.StatusCode, message)
+		return
+	}
+}
+
+func editIssue(owner, repository, accessToken, number, title string) {
+	req, err := http.NewRequest("PATCH", gitHubAPIRoot+"/"+owner+"/"+repository+"/issues/"+number, strings.NewReader(`{"title":"`+title+`"}`))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Set("Authorization", "token "+accessToken)
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		var message struct{ Message string }
+		if err := json.NewDecoder(resp.Body).Decode(&message); err != nil {
+			fmt.Printf("failed to decode response body as json. error: %v", err)
+		}
+		fmt.Printf("failed to request. status code: %v. reason: %v", resp.StatusCode, message)
+		return
+	}
+}
+
+func closeIssue(owner, repository, accessToken, number string) {
+	req, err := http.NewRequest("PATCH", gitHubAPIRoot+"/"+owner+"/"+repository+"/issues/"+number, strings.NewReader(`{"state":"closed"}`))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Set("Authorization", "token "+accessToken)
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		var message struct{ Message string }
+		if err := json.NewDecoder(resp.Body).Decode(&message); err != nil {
+			fmt.Printf("failed to decode response body as json. error: %v", err)
+		}
+		fmt.Printf("failed to request. status code: %v. reason: %v", resp.StatusCode, message)
+		return
+	}
 }
