096da816d1626d18394b06f87dbf8a1ad369b41a
Ken-Miura <ken.miura1102@gmail.com>
2017-05-14 03:21:07 +0900

    練習問題追加

---
+++ ch04/ex12/main.go
@@ -0,0 +1,133 @@
+// Copyright 2017 Ken Mirua
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const xkcdURL = "https://xkcd.com/%d"
+const xkcdAPIRoot = xkcdURL + "/info.0.json"
+
+var operation = flag.String("o", "search", "operations for xkcd: create or search")
+
+func main() {
+	flag.Parse()
+	if !(*operation == "create" || *operation == "search") {
+		fmt.Println("Operation must be create or search.")
+		return
+	}
+
+	programName := os.Args[0]
+	args := flag.Args()
+	if *operation == "create" {
+		createIndex()
+	} else if *operation == "search" {
+		if len(args) != 1 {
+			fmt.Println("usage: " + programName + " [-o search] 'word'")
+			fmt.Println("ex1. " + programName + " Barrel")
+			fmt.Println("ex2. " + programName + " -o search Barrel")
+			return
+		}
+
+		f, err := os.Open("index.json")
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println("Index file to search comic might not exist.")
+			fmt.Println("Please create index file at first.")
+			fmt.Println("how to create: " + programName + " -o create")
+			return
+		}
+		var indexes []comicIndex
+		ok := constructIndex(f, &indexes)
+		if !ok {
+			fmt.Println("failed to construct index")
+			fmt.Println("Please re-create index file.")
+			fmt.Println("how to create: " + programName + " -o create")
+			return
+		}
+		searchComic(indexes, args[0])
+	} else {
+		panic("This line must not be reached.\n")
+	}
+}
+
+func createIndex() {
+	f, err := os.Create("index.json")
+	if err != nil {
+		fmt.Printf("failed to create file. error: %v\n", err)
+		return
+	}
+	defer f.Close()
+
+	var indexes []comicIndex
+	var ok bool = true
+	for i := 1; ok; i++ {
+		if i == 404 { // this numbering results in Not Found
+			continue
+		}
+		var index comicIndex
+		index, ok = requestIndex(fmt.Sprintf(xkcdAPIRoot, i))
+		if ok {
+			indexes = append(indexes, index)
+		}
+	}
+
+	if err := json.NewEncoder(f).Encode(indexes); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("succeeded in creating index")
+}
+
+func requestIndex(url string) (comicIndex, bool) {
+	fmt.Printf("fetching from: %s\n", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return comicIndex{}, false
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return comicIndex{}, false
+	}
+
+	var index comicIndex
+	if err := json.NewDecoder(resp.Body).Decode(&index); err != nil {
+		fmt.Println(err)
+		return comicIndex{}, false
+	}
+	return index, true
+}
+
+func constructIndex(r io.Reader, indexes *[]comicIndex) bool {
+	if err := json.NewDecoder(r).Decode(&indexes); err != nil {
+		fmt.Println(err)
+		return false
+	}
+	return true
+}
+
+func searchComic(indexes []comicIndex, word string) {
+	for _, index := range indexes {
+		if strings.Contains(index.Title, word) || strings.Contains(index.Transcript, word) {
+			fmt.Printf("title: %s\n", index.Title)
+			fmt.Printf("URL: %s\n", fmt.Sprintf(xkcdURL, index.Num))
+			fmt.Printf("transcript:\n%s\n", index.Transcript)
+			return
+		}
+	}
+	fmt.Printf("Not found %s in index\n", word)
+}
+
+type comicIndex struct {
+	Num        int    `json:"num"`
+	Title      string `json:"title"`
+	Transcript string `json:"transcript"`
+}
---
+++ ch04/ex12/runall.bat
@@ -0,0 +1,2 @@
+go run main.go -o create
+go run main.go Barrel
