f08f312dcc3c58b7bb1865d5dcc704ef5294c344
Ken-Miura <ken.miura1102@gmail.com>
2017-05-13 21:19:07 +0900

    メモ追記

--- ch04/ex07/reverse.go
+++ ch04/ex07/reverse.go
@@ -1,7 +1,7 @@
 // Copyright 2017 Ken Mirua
 package ex07
 
-// TODO 追加のメモリ割り当てなしのバージョン
+// TODO 追加のメモリ割り当てなしのバージョン。UTF-8が可変長エンコーディングなせいで多分無理そうな気がする。
 func Reverse(s []byte) []byte {
 	runes := []rune(string(s))
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
