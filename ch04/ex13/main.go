6e28fc98e45962bb3c18c25cca4cf604f8c6889c
Ken-Miura <ken.miura1102@gmail.com>
2017-05-14 19:09:53 +0900

    出力先を標準出力に変更

--- ch04/ex13/main.go
+++ ch04/ex13/main.go
@@ -14,44 +14,37 @@ const APIRoot = "http://www.omdbapi.com/?t="
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println(`usage: ` + os.Args[0] + ` "movie title"`)
-		fmt.Println(`ex. ` + os.Args[0] + ` "The Lord of the Rings: The Fellowship of the Ring"`)
+		fmt.Fprintln(os.Stderr, `usage: `+os.Args[0]+` "movie title"`)
+		fmt.Fprintln(os.Stderr, `ex. `+os.Args[0]+` "The Lord of the Rings: The Fellowship of the Ring"`)
 		return
 	}
 
 	q := url.QueryEscape(os.Args[1])
 	resp1, err := http.Get(APIRoot + q)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Fprintln(os.Stderr, err)
 		return
 	}
 	defer resp1.Body.Close()
 
 	var info movieInfo
 	if err := json.NewDecoder(resp1.Body).Decode(&info); err != nil {
-		fmt.Printf("failed to decode response as json. error: %v", err)
+		fmt.Fprintf(os.Stderr, "failed to decode response as json. error: %v\n", err)
 		return
 	}
 
 	if info.Response == "False" {
-		fmt.Println("Not found")
+		fmt.Fprintf(os.Stderr, "title (%s) not found\n", os.Args[1])
 		return
 	}
 
 	resp2, err := http.Get(info.Poster)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Fprintln(os.Stderr, err)
 		return
 	}
 	defer resp2.Body.Close()
-
-	f, err := os.Create("poster.jpg") // 画像ファイルの拡張子の仕様はjpgで固定？？？
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer f.Close()
-	io.Copy(f, resp2.Body)
+	io.Copy(os.Stdout, resp2.Body)
 }
 
 type movieInfo struct {
