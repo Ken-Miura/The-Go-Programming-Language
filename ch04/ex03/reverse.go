dfe877fa13f5ff1ef2a6f525bddd78bdd0e839e8
Ken-Miura <ken.miura1102@gmail.com>
2017-05-19 02:48:02 +0900

    TODO追記

--- ch04/ex03/reverse.go
+++ ch04/ex03/reverse.go
@@ -1,6 +1,7 @@
 // Copyright 2017 Ken Mirua
 package ex03
 
+// TODO func Reverse(s *[...]int) では駄目？？？関数の定義のコンパイルは通るが、実際に関数を使うコードは、実引数を与える部分でコンパイルエラーになる？？？なんで？？？
 func Reverse(s *[10]int) {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
