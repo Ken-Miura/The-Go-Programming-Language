b27fad61cd70c90ff18cf5424d78bf1400ddab40
Ken-Miura <ken.miura1102@gmail.com>
2017-05-20 12:01:34 +0900

    TODO削除

--- ch04/ex07/reverse.go
+++ ch04/ex07/reverse.go
@@ -1,11 +1,20 @@
 // Copyright 2017 Ken Mirua
 package ex07
 
-// TODO 追加のメモリ割り当てなしのバージョンの検討
+import "unicode/utf8"
+
 func Reverse(s []byte) []byte {
-	runes := []rune(string(s))
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
+	size := 0
+	for size < len(s) {
+		_, runeLength := utf8.DecodeRune(s[size:])
+		for i, j := size, (size+runeLength)-1; i < j; i, j = i+1, j-1 {
+			s[i], s[j] = s[j], s[i]
+		}
+		size += runeLength
 	}
-	return []byte(string(runes))
+
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+	return s
 }
