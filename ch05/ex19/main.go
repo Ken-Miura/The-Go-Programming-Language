3f46073ec33fba23661e7aeaf6d604ac03bb3f8a
Ken-Miura <ken.miura1102@gmail.com>
2017-05-31 02:13:32 +0900

    練習問題追加

---
+++ ch05/ex19/main.go
@@ -0,0 +1,29 @@
+// Copyright 2017 Ken Mirua
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s := withoutReturn()
+	if s == "" {
+		fmt.Printf("withoutReturn returns zero value: %s\n", s)
+		os.Exit(1)
+	}
+	fmt.Printf("withoutReturn returns non zero value: %s\n", s)
+}
+
+func withoutReturn() (result string) {
+	type bailout struct{}
+	defer func() {
+		switch p := recover(); p {
+		case bailout{}:
+			result = "non zero value of type string"
+		default:
+			panic(p)
+		}
+	}()
+	panic(bailout{})
+}
