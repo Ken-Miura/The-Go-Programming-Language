8fc9435b161f9e331be334e4d0a98e98962c215d
Ken-Miura <ken.miura1102@gmail.com>
2017-05-30 21:36:16 +0900

    練習問題追加

---
+++ ch06/ex01/intset.go
@@ -0,0 +1,108 @@
+// Copyright 2017 Ken Mirua
+package ex01
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type IntSet struct {
+	words []uint64
+}
+
+func (s *IntSet) Len() int {
+	num := 0
+	if s == nil {
+		return num
+	}
+	for i := range s.words {
+		num += bitCount(s.words[i])
+	}
+	return num
+}
+
+// ch2のサンプルコードより
+func bitCount(x uint64) int {
+	// Hacker's Delight, Figure 5-2.
+	x = x - ((x >> 1) & 0x5555555555555555)
+	x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333)
+	x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f
+	x = x + (x >> 8)
+	x = x + (x >> 16)
+	x = x + (x >> 32)
+	return int(x & 0x7f)
+}
+
+func (s *IntSet) Remove(x int) {
+	if s == nil {
+		return
+	}
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		return
+	}
+	s.words[word] &^= 1 << bit
+}
+
+func (s *IntSet) Clear() {
+	if s == nil {
+		return
+	}
+	for i := range s.words {
+		s.words[i] &^= 63
+	}
+}
+
+func (s *IntSet) Copy() *IntSet {
+	if s == nil {
+		return &IntSet{}
+	}
+	copied := &IntSet{words: make([]uint64, len(s.words), cap(s.words))}
+	for i := range s.words {
+		copied.words[i] = s.words[i]
+	}
+	return copied
+}
+
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
---
+++ ch06/ex01/intset_test.go
@@ -0,0 +1,101 @@
+// Copyright 2017 Ken Mirua
+package ex01
+
+import "testing"
+
+func TestIntSet_Len(t *testing.T) {
+	tests := []struct {
+		receiver IntSet
+		expected int
+	}{
+		{IntSet{[]uint64{0}}, 0},
+		{IntSet{[]uint64{1}}, 1},
+		{IntSet{[]uint64{3}}, 2},
+		{IntSet{[]uint64{0, 1}}, 1},
+		{IntSet{[]uint64{1, 1, 1, 1}}, 4},
+		{IntSet{[]uint64{4, 0, 5, 3}}, 5},
+	}
+
+	for _, test := range tests {
+		actual := test.receiver.Len()
+		if actual != test.expected {
+			t.Fatalf("(*IntSet).Len() failed: input is %d but actual is %d", test.expected, actual)
+		}
+	}
+}
+
+func TestIntSet_Remove(t *testing.T) {
+	tests := []struct {
+		receiver IntSet
+		input    int
+		expected IntSet
+	}{
+		{IntSet{[]uint64{0}}, 0, IntSet{[]uint64{0}}},
+		{IntSet{[]uint64{2}}, 1, IntSet{[]uint64{0}}},
+		{IntSet{[]uint64{3}}, 2, IntSet{[]uint64{3}}},
+		{IntSet{[]uint64{0: 0, 1: 1}}, 64, IntSet{[]uint64{0: 0, 1: 0}}},
+		{IntSet{[]uint64{0: 1, 1: 1, 2: 0, 3: 1}}, 128, IntSet{[]uint64{0: 1, 1: 1, 2: 0, 3: 1}}},
+	}
+
+	for _, test := range tests {
+		test.receiver.Remove(test.input)
+		if len(test.receiver.words) != len(test.expected.words) {
+			t.Fatalf("(*IntSet).Remove(int) failed: expected is %v but actual is %v", test.expected, test.receiver)
+		}
+		for i := range test.receiver.words {
+			if test.receiver.words[i] != test.expected.words[i] {
+				t.Fatalf("(*IntSet).Remove(int) failed: expected is %v but actual is %v", test.expected, test.receiver)
+			}
+		}
+	}
+}
+
+func TestIntSet_Clear(t *testing.T) {
+	tests := []struct {
+		receiver IntSet
+		expected IntSet
+	}{
+		{IntSet{[]uint64{0}}, IntSet{[]uint64{0}}},
+		{IntSet{[]uint64{2}}, IntSet{[]uint64{0}}},
+		{IntSet{[]uint64{3}}, IntSet{[]uint64{0}}},
+		{IntSet{[]uint64{0, 1}}, IntSet{[]uint64{0, 0}}},
+		{IntSet{[]uint64{1, 1, 1, 1}}, IntSet{[]uint64{0, 0, 0, 0}}},
+	}
+
+	for _, test := range tests {
+		test.receiver.Clear()
+		if len(test.receiver.words) != len(test.expected.words) {
+			t.Fatalf("(*IntSet).Clear() failed: expected is %v but actual is %v", test.expected, test.receiver)
+		}
+		for i := range test.receiver.words {
+			if test.receiver.words[i] != test.expected.words[i] {
+				t.Fatalf("(*IntSet).Clear() failed: expected is %v but actual is %v", test.expected, test.receiver)
+			}
+		}
+	}
+}
+
+func TestIntSet_Copy(t *testing.T) {
+	tests := []struct {
+		receiver IntSet
+		expected IntSet
+	}{
+		{IntSet{[]uint64{0}}, IntSet{[]uint64{0}}},
+		{IntSet{[]uint64{2}}, IntSet{[]uint64{2}}},
+		{IntSet{[]uint64{3}}, IntSet{[]uint64{3}}},
+		{IntSet{[]uint64{0, 1}}, IntSet{[]uint64{0, 1}}},
+		{IntSet{[]uint64{1, 1, 1, 1}}, IntSet{[]uint64{1, 1, 1, 1}}},
+	}
+
+	for _, test := range tests {
+		actual := test.receiver.Copy()
+		if len(actual.words) != len(test.expected.words) {
+			t.Fatalf("(*IntSet).Copy() failed: expected is %v but actual is %v", test.expected, actual)
+		}
+		for i := range actual.words {
+			if actual.words[i] != test.expected.words[i] {
+				t.Fatalf("(*IntSet).Copy() failed: expected is %v but actual is %v", test.expected, actual)
+			}
+		}
+	}
+}
