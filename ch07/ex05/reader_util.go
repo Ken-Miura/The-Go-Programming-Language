4fb803aa6be934aa1cf8970c41cefceb59665659
Ken-Miura <ken.miura1102@gmail.com>
2017-06-01 05:17:37 +0900

    練習問題追加

---
+++ ch07/ex05/reader_util.go
@@ -0,0 +1,25 @@
+// Copyright 2017 Ken Mirua
+package ex05
+
+import "io"
+
+func LimitReader(r io.Reader, n int64) io.Reader {
+	return &limitReader{r, n, 0}
+}
+
+type limitReader struct {
+	r    io.Reader
+	n    int64
+	read int64
+}
+
+var _ io.Reader = (*limitReader)(nil)
+
+func (lr *limitReader) Read(p []byte) (int, error) {
+	nbytes, err := lr.r.Read(p)
+	if (lr.read + int64(nbytes)) >= lr.n {
+		return int(lr.n - lr.read), io.EOF
+	}
+	lr.read += int64(nbytes)
+	return nbytes, err
+}
---
+++ ch07/ex05/reader_util_test.go
@@ -0,0 +1,37 @@
+// Copyright 2017 Ken Mirua
+package ex05
+
+import (
+	"bytes"
+	"io"
+	"strings"
+	"testing"
+)
+
+var tests = []struct {
+	input1    string
+	input2    int64
+	expected1 string
+	expected2 int64
+}{
+	{"", 1, "", 0},
+	{"hello", 4, "hell", 4},
+	{"hello", 5, "hello", 5},
+	{"hello", 6, "hello", 5},
+	{"hello", 0, "", 0},
+}
+
+func TestLimitReader(t *testing.T) {
+
+	for _, test := range tests {
+		r := LimitReader(strings.NewReader(test.input1), test.input2)
+		var out bytes.Buffer
+		nbytes, _ := io.Copy(&out, r)
+		if out.String() != test.expected1 {
+			t.Fatalf("%s is expected but result is %s", test.expected1, out.String())
+		}
+		if nbytes != test.expected2 {
+			t.Fatalf("%d is expected but result is %d", test.expected2, nbytes)
+		}
+	}
+}
