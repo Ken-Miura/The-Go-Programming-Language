7911de70144f93a6a01cdb79277051c209899acd
Ken-Miura <ken.miura1102@gmail.com>
2017-06-01 02:30:29 +0900

    練習問題追加

---
+++ ch07/ex02/counter.go
@@ -0,0 +1,22 @@
+// Copyright 2017 Ken Mirua
+package ex02
+
+import "io"
+
+func CountingWriter(w io.Writer) (io.Writer, *int64) {
+	counter := byteCounter{w, 0}
+	return &counter, &(counter.count)
+}
+
+type byteCounter struct {
+	w     io.Writer
+	count int64
+}
+
+var _ io.Writer = (*byteCounter)(nil)
+
+func (c *byteCounter) Write(p []byte) (int, error) {
+	nbytes, err := c.w.Write(p)
+	(*c).count += int64(nbytes)
+	return nbytes, err
+}
