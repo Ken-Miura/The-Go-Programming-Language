9e0ed63f6ff24287d4f4f08dd666caf59895e39a
Ken-Miura <ken.miura1102@gmail.com>
2017-06-10 20:24:42 +0900

    32bitと64bitの両方でビルドできるように修正

--- ch06/ex05/intset.go
+++ ch06/ex05/intset.go
@@ -65,21 +65,19 @@ func (s *IntSet) Len() int {
 		return num
 	}
 	for i := range s.words {
-		num += bitCount(s.words[i])
+		num += popCountByClearing(s.words[i])
 	}
 	return num
 }
 
-// ch2のサンプルコードより
-func bitCount(x uint) int {
-	// Hacker's Delight, Figure 5-2.
-	x = x - ((x >> 1) & 0x5555555555555555)
-	x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333)
-	x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f
-	x = x + (x >> 8)
-	x = x + (x >> 16)
-	x = x + (x >> 32)
-	return int(x & 0x7f)
+// ch2より引数の型を変更して利用
+func popCountByClearing(x uint) int {
+	n := 0
+	for x != 0 {
+		x = x & (x - 1) // clear rightmost non-zero bit
+		n++
+	}
+	return n
 }
 
 func (s *IntSet) Remove(x int) {
