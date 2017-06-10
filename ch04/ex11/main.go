9040d225db6eb67f764b903a6c8f45d21616db35
Ken-Miura <ken.miura1102@gmail.com>
2017-06-10 13:31:53 +0900

    エディタ連携のイシュー作成処理ほぼ完成（エディタで改行コードを保存した際のJSONパースエラーがまだ未対応）

--- ch04/ex11/main.go
+++ ch04/ex11/main.go
@@ -2,11 +2,14 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
 	"strings"
 	"time"
 )
@@ -17,6 +20,7 @@ var operation = flag.String("o", "list", "issue operation for the repository: li
 
 // TODO エディタ連携 エディタを起動した後にそのエディタに入力された文字列はどうやって取得すればよい？？？
 func main() {
+	//createIssue("Ken-Miura", "GitHub-API-Practice", "997a0cc0c949edd2a288736294d2375ea55917c0", "test6", "")
 	flag.Parse()
 	if !(*operation == "list" || *operation == "create" || *operation == "edit" || *operation == "close") {
 		fmt.Println("Operation must be list, create, edit or close.")
@@ -35,13 +39,65 @@ func main() {
 		listIssues(args[0], args[1])
 		return
 	} else if *operation == "create" {
-		if len(args) != 4 {
-			fmt.Println("usage: " + programName + " -o create 'owner' 'repository' 'access token' 'title'")
-			fmt.Println("ex. " + programName + " -o create Ken-Miura GitHub-API-Practice xxxxxxxxxx issue1")
+		if !(len(args) == 4 || len(args) == 5) {
+			fmt.Println("usage1: " + programName + " -o create 'owner' 'repository' 'access token' 'title' 'body'")
+			fmt.Println("ex. " + programName + ` -o create Ken-Miura GitHub-API-Practice xxxxxxxxxx issue1 "this is description""`)
+			fmt.Println("usage2: " + programName + " -o create 'owner' 'repository' 'access token' 'editor path'")
+			fmt.Println("ex. " + programName + " -o create Ken-Miura GitHub-API-Practice xxxxxxxxxx vim")
+			fmt.Println("caution: Save contents as UTF-8 If you use editor.")
 			return
 		}
-		createIssue(args[0], args[1], args[2], args[3])
-		return
+		if len(args) == 4 {
+			// title取得処理
+			fmt.Printf("issue title: ")
+			scanner := bufio.NewScanner(os.Stdin)
+			title := ""
+			for scanner.Scan() {
+				title = scanner.Text()
+				if title != "" {
+					break
+				}
+				fmt.Println("Enter issue title.")
+				fmt.Printf("issue title: ")
+			}
+			// ここまでtitle取得処理
+
+			// body取得処理
+			fmt.Printf("Edit issue description with editor. After editing, save and close editor.\n")
+			f, err := ioutil.TempFile("", "issue_util")
+			if err != nil {
+				fmt.Printf("failed to create temp file to edit issue description for editor use: %v", err)
+				return
+			}
+			tempFilePath := f.Name()
+			f.Close()
+			defer func() {
+				err = os.Remove(tempFilePath)
+				if err != nil {
+					fmt.Printf("failed to delete temp file (%s): %v", tempFilePath, err)
+				}
+			}()
+			cmd := exec.Command(args[3], tempFilePath)
+			err = cmd.Run()
+			if err != nil {
+				fmt.Printf("failed to execute %s: %v", args[2], err)
+				return
+			}
+			content, err := ioutil.ReadFile(tempFilePath)
+			if err != nil {
+				fmt.Printf("failed to read temp file (%s): %v", tempFilePath, err)
+				return
+			}
+			// ここまでbody取得処理
+
+			createIssue(args[0], args[1], args[2], title, string(content))
+			return
+		} else if len(args) == 5 {
+			createIssue(args[0], args[1], args[2], args[3], args[4])
+			return
+		} else {
+			panic("This line must not be reached.\n")
+		}
 	} else if *operation == "edit" {
 		if len(args) != 5 {
 			fmt.Println("usage: " + programName + " -o edit 'owner' 'repository' 'access token' 'issue No.' 'new title'")
@@ -102,8 +158,8 @@ func listIssues(owner, repository string) {
 	}
 }
 
-func createIssue(owner, repository, accessToken, title string) {
-	req, err := http.NewRequest("POST", gitHubAPIRoot+"/"+owner+"/"+repository+"/issues", strings.NewReader(`{"title":"`+title+`"}`))
+func createIssue(owner, repository, accessToken, title, body string) {
+	req, err := http.NewRequest("POST", gitHubAPIRoot+"/"+owner+"/"+repository+"/issues", strings.NewReader(`{"title":"`+title+`","body":"`+body+`"}`))
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -120,7 +176,7 @@ func createIssue(owner, repository, accessToken, title string) {
 	if resp.StatusCode != 201 {
 		var message struct{ Message string }
 		if err := json.NewDecoder(resp.Body).Decode(&message); err != nil {
-			fmt.Printf("failed to decode response body as json. error: %v", err)
+			fmt.Printf("failed to decode response body as json: %v", err)
 		}
 		fmt.Printf("failed to request. status code: %v. reason: %v", resp.StatusCode, message)
 		return
@@ -145,7 +201,7 @@ func editIssue(owner, repository, accessToken, number, title string) {
 	if resp.StatusCode != http.StatusOK {
 		var message struct{ Message string }
 		if err := json.NewDecoder(resp.Body).Decode(&message); err != nil {
-			fmt.Printf("failed to decode response body as json. error: %v", err)
+			fmt.Printf("failed to decode response body as json: %v", err)
 		}
 		fmt.Printf("failed to request. status code: %v. reason: %v", resp.StatusCode, message)
 		return
@@ -170,7 +226,7 @@ func closeIssue(owner, repository, accessToken, number string) {
 	if resp.StatusCode != http.StatusOK {
 		var message struct{ Message string }
 		if err := json.NewDecoder(resp.Body).Decode(&message); err != nil {
-			fmt.Printf("failed to decode response body as json. error: %v", err)
+			fmt.Printf("failed to decode response body as json: %v", err)
 		}
 		fmt.Printf("failed to request. status code: %v. reason: %v", resp.StatusCode, message)
 		return
