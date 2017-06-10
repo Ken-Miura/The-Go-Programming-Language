d6820115c3de68aa228e394260f38fd02c1738da
Ken-Miura <ken.miura1102@gmail.com>
2017-06-10 15:15:19 +0900

    TODO削除

--- ch04/ex11/main.go
+++ ch04/ex11/main.go
@@ -18,9 +18,7 @@ const gitHubAPIRoot = "https://api.github.com/repos"
 
 var operation = flag.String("o", "list", "issue operation for the repository: list, create, edit or close")
 
-// TODO エディタ連携 エディタを起動した後にそのエディタに入力された文字列はどうやって取得すればよい？？？
 func main() {
-	//createIssue("Ken-Miura", "GitHub-API-Practice", "997a0cc0c949edd2a288736294d2375ea55917c0", "test6", "")
 	flag.Parse()
 	if !(*operation == "list" || *operation == "create" || *operation == "edit" || *operation == "close") {
 		fmt.Println("Operation must be list, create, edit or close.")
@@ -48,56 +46,12 @@ func main() {
 			return
 		}
 		if len(args) == 4 {
-			// title取得処理
-			fmt.Printf("issue title: ")
-			scanner := bufio.NewScanner(os.Stdin)
-			title := ""
-			for scanner.Scan() {
-				title = scanner.Text()
-				if title != "" {
-					break
-				}
-				fmt.Println("Enter issue title.")
-				fmt.Printf("issue title: ")
-			}
-			// ここまでtitle取得処理
-
-			// body取得処理
-			fmt.Printf("Edit issue description with editor. After editing, save and close editor.\n")
-			f, err := ioutil.TempFile("", "issue_util")
+			title := promptAndGetTitle()
+			body, err := promptWithEditorAndGetBody(args[3])
 			if err != nil {
-				fmt.Printf("failed to create temp file to edit issue description for editor use: %v", err)
+				fmt.Printf("failed to get issue description: %v", err)
 				return
 			}
-			tempFilePath := f.Name()
-			f.Close()
-			defer func() {
-				err = os.Remove(tempFilePath)
-				if err != nil {
-					fmt.Printf("failed to delete temp file (%s): %v", tempFilePath, err)
-				}
-			}()
-			cmd := exec.Command(args[3], tempFilePath)
-			err = cmd.Run()
-			if err != nil {
-				fmt.Printf("failed to execute %s: %v", args[2], err)
-				return
-			}
-			content, err := ioutil.ReadFile(tempFilePath)
-			if err != nil {
-				fmt.Printf("failed to read temp file (%s): %v", tempFilePath, err)
-				return
-			}
-			body := ""
-			for _, s := range string(content) { // JSONにおいて、文字列の中で改行コードを含める際は\\nを使う。なので\r\nについて置き換える。
-				if s == '\r' || s == '\n' {
-					body += `\\n`
-					continue
-				}
-				body += string(s)
-			}
-			// ここまでbody取得処理
-
 			createIssue(args[0], args[1], args[2], title, body)
 			return
 		} else if len(args) == 5 {
@@ -107,13 +61,28 @@ func main() {
 			panic("This line must not be reached.\n")
 		}
 	} else if *operation == "edit" {
-		if len(args) != 5 {
-			fmt.Println("usage: " + programName + " -o edit 'owner' 'repository' 'access token' 'issue No.' 'new title'")
-			fmt.Println("ex. " + programName + " -o edit Ken-Miura GitHub-API-Practice xxxxxxxxxx 2 ISSUE")
+		if !(len(args) == 5 || len(args) == 6) {
+			fmt.Println("usage1: " + programName + " -o edit 'owner' 'repository' 'access token' 'issue No.' 'new title' 'new description'")
+			fmt.Println("ex. " + programName + ` -o edit Ken-Miura GitHub-API-Practice xxxxxxxxxx 2 ISSUE "this is new description"`)
+			fmt.Println("usage2: " + programName + " -o edit 'owner' 'repository' 'access token' 'issue No.' 'editor path'")
+			fmt.Println("ex. " + programName + ` -o edit Ken-Miura GitHub-API-Practice xxxxxxxxxx 2 vim`)
 			return
 		}
-		editIssue(args[0], args[1], args[2], args[3], args[4])
-		return
+		if len(args) == 5 {
+			title := promptAndGetTitle()
+			body, err := promptWithEditorAndGetBody(args[4])
+			if err != nil {
+				fmt.Printf("failed to get issue description: %v", err)
+				return
+			}
+			editIssue(args[0], args[1], args[2], args[3], title, body)
+			return
+		} else if len(args) == 6 {
+			editIssue(args[0], args[1], args[2], args[3], args[4], args[5])
+			return
+		} else {
+			panic("This line must not be reached.\n")
+		}
 	} else if *operation == "close" {
 		if len(args) != 4 {
 			fmt.Println("usage: " + programName + " -o close 'owner' 'repository' 'access token' 'issue No.'")
@@ -191,8 +160,8 @@ func createIssue(owner, repository, accessToken, title, body string) {
 	}
 }
 
-func editIssue(owner, repository, accessToken, number, title string) {
-	req, err := http.NewRequest("PATCH", gitHubAPIRoot+"/"+owner+"/"+repository+"/issues/"+number, strings.NewReader(`{"title":"`+title+`"}`))
+func editIssue(owner, repository, accessToken, number, title, body string) {
+	req, err := http.NewRequest("PATCH", gitHubAPIRoot+"/"+owner+"/"+repository+"/issues/"+number, strings.NewReader(`{"title":"`+title+`","body":"`+body+`"}`))
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -239,4 +208,52 @@ func closeIssue(owner, repository, accessToken, number string) {
 		fmt.Printf("failed to request. status code: %v. reason: %v", resp.StatusCode, message)
 		return
 	}
+}
+
+func promptAndGetTitle() string {
+	fmt.Printf("issue title: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	title := ""
+	for scanner.Scan() {
+		title = scanner.Text()
+		if title != "" {
+			break
+		}
+		fmt.Println("Enter issue title.")
+		fmt.Printf("issue title: ")
+	}
+	return title
+}
+
+func promptWithEditorAndGetBody(editorPath string) (string, error) {
+	fmt.Printf("Edit issue description with editor. After editing, save and close editor.\n")
+	f, err := ioutil.TempFile("", "issue_util")
+	if err != nil {
+		return "", fmt.Errorf("failed to create temp file to edit issue description for editor use: %v", err)
+	}
+	tempFilePath := f.Name()
+	f.Close()
+	defer func() {
+		if removeErr := os.Remove(tempFilePath); removeErr != nil {
+			fmt.Printf("failed to delete temp file (%s): %v\n", tempFilePath, removeErr)
+		}
+	}()
+	cmd := exec.Command(editorPath, tempFilePath)
+	err = cmd.Run()
+	if err != nil {
+		return "", fmt.Errorf("failed to execute %s: %v", editorPath, err)
+	}
+	content, err := ioutil.ReadFile(tempFilePath)
+	if err != nil {
+		return "", fmt.Errorf("failed to read temp file (%s): %v", tempFilePath, err)
+	}
+	body := ""
+	for _, s := range string(content) { // JSONにおいて、文字列の中で改行コードを含める際は\\nを使う。なので\r\nについて置き換える。
+		if s == '\r' || s == '\n' {
+			body += `\\n`
+			continue
+		}
+		body += string(s)
+	}
+	return body, nil
 }
