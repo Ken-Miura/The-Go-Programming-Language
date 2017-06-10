8c287b019ac89134dfb4f025a97ad06335aa2e37
Ken-Miura <ken.miura1102@gmail.com>
2017-06-10 22:17:50 +0900

    外部公開APIテストになるように修正

--- ch06/ex02/intset_test.go
+++ ch06/ex02/intset_test.go
@@ -1,95 +1,106 @@
 // Copyright 2017 Ken Miura
-package ex02
+package ex02_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/Ken-Miura/The-Go-Programming-Language/ch06/ex02"
+)
 
 func TestIntSet_AddAll(t *testing.T) {
 
-	receiver1 := IntSet{[]uint64{0}}
-	expected1 := IntSet{[]uint64{14}}
-
-	receiver1.AddAll(1, 2, 3)
-
-	if len(receiver1.words) != len(expected1.words) {
-		t.Fatalf("(*IntSet).AddAll() failed: expected is %v but actual is %v", expected1, receiver1)
+	tests := []struct {
+		receiver *ex02.IntSet
+		input    []int
+		expected []int
+	}{
+		{ex02.NewIntSet(), []int{1, 2, 3}, []int{1, 2, 3}},
+		{ex02.NewIntSet(), []int{}, []int{}},
+		{ex02.NewIntSet(), []int{64}, []int{64}},
+		{ex02.NewIntSet(0), []int{64}, []int{0, 64}},
 	}
-	for i := range receiver1.words {
-		if receiver1.words[i] != expected1.words[i] {
-			t.Fatalf("(*IntSet).AddAll() failed: expected is %v but actual is %v", expected1, receiver1)
+
+	for _, test := range tests {
+		test.receiver.AddAll(test.input...)
+		if test.receiver.Len() != len(test.expected) {
+			t.Fatalf("(*ex02.IntSet).Len() in (*ex02.IntSet).AddAll(...int) failed: expected is %d but actual is %d", len(test.expected), test.receiver.Len())
+		}
+		for _, integer := range test.expected {
+			if !test.receiver.Has(integer) {
+				t.Fatalf("(*ex02.IntSet).Has(int) in *ex02.IntSet).AddAll(...int) failed: receiver is expected to have %d", integer)
+			}
 		}
 	}
+}
 
-	receiver2 := IntSet{[]uint64{0}}
-	expected2 := IntSet{[]uint64{0}}
-
-	receiver2.AddAll()
-
-	if len(receiver2.words) != len(expected2.words) {
-		t.Fatalf("(*IntSet).AddAll() failed: expected is %v but actual is %v", expected2, receiver2)
+func TestNewIntSet(t *testing.T) {
+	tests := []struct {
+		integers []int
+	}{
+		{[]int{}},
+		{[]int{0}},
+		{[]int{1}},
+		{[]int{1, 2, 3, 4}},
+		{[]int{63}},
+		{[]int{64}},
+		{[]int{127, 128}},
 	}
-	for i := range receiver2.words {
-		if receiver2.words[i] != expected2.words[i] {
-			t.Fatalf("(*IntSet).AddAll() failed: expected is %v but actual is %v", expected2, receiver2)
+
+	for _, test := range tests {
+		actual := ex02.NewIntSet(test.integers...)
+		if actual.Len() != len(test.integers) {
+			t.Fatalf("(*ex02.IntSet).Len() in ex02.NewIntSet failed: expected is %d but actual is %d", len(test.integers), actual.Len())
 		}
-	}
-
-	receiver3 := IntSet{[]uint64{0}}
-	expected3 := IntSet{[]uint64{0, 1}}
-
-	receiver3.AddAll(64)
-
-	if len(receiver3.words) != len(expected3.words) {
-		t.Fatalf("(*IntSet).AddAll() failed: expected is %v but actual is %v", expected3, receiver3)
-	}
-	for i := range receiver3.words {
-		if receiver3.words[i] != expected3.words[i] {
-			t.Fatalf("(*IntSet).AddAll() failed: expected is %v but actual is %v", expected3, receiver3)
+		for _, integer := range test.integers {
+			if !actual.Has(integer) {
+				t.Fatalf("(*ex02.IntSet).Has(int) in ex02.NewIntSet failed: actual is expected to have %d", integer)
+			}
 		}
 	}
 }
 
 func TestIntSet_Len(t *testing.T) {
 	tests := []struct {
-		receiver IntSet
+		receiver *ex02.IntSet
 		expected int
 	}{
-		{IntSet{[]uint64{0}}, 0},
-		{IntSet{[]uint64{1}}, 1},
-		{IntSet{[]uint64{3}}, 2},
-		{IntSet{[]uint64{0, 1}}, 1},
-		{IntSet{[]uint64{1, 1, 1, 1}}, 4},
-		{IntSet{[]uint64{4, 0, 5, 3}}, 5},
+		{ex02.NewIntSet(), 0},
+		{ex02.NewIntSet(0), 1},
+		{ex02.NewIntSet(0, 1), 2},
+		{ex02.NewIntSet(64), 1},
+		{ex02.NewIntSet(64, 128, 192, 256), 4},
+		{ex02.NewIntSet(2, 2), 1},
 	}
 
 	for _, test := range tests {
 		actual := test.receiver.Len()
 		if actual != test.expected {
-			t.Fatalf("(*IntSet).Len() failed: input is %d but actual is %d", test.expected, actual)
+			t.Fatalf("(*ex02.IntSet).Len() failed: expected is %d but actual is %d", test.expected, actual)
 		}
 	}
 }
 
 func TestIntSet_Remove(t *testing.T) {
 	tests := []struct {
-		receiver IntSet
+		receiver *ex02.IntSet
 		input    int
-		expected IntSet
+		expected []int
 	}{
-		{IntSet{[]uint64{0}}, 0, IntSet{[]uint64{0}}},
-		{IntSet{[]uint64{2}}, 1, IntSet{[]uint64{0}}},
-		{IntSet{[]uint64{3}}, 2, IntSet{[]uint64{3}}},
-		{IntSet{[]uint64{0: 0, 1: 1}}, 64, IntSet{[]uint64{0: 0, 1: 0}}},
-		{IntSet{[]uint64{0: 1, 1: 1, 2: 0, 3: 1}}, 128, IntSet{[]uint64{0: 1, 1: 1, 2: 0, 3: 1}}},
+		{ex02.NewIntSet(0), 0, []int{}},
+		{ex02.NewIntSet(1), 1, []int{}},
+		{ex02.NewIntSet(0, 1), 2, []int{0, 1}},
+		{ex02.NewIntSet(64), 64, []int{}},
+		{ex02.NewIntSet(0, 64, 192), 128, []int{0, 64, 192}},
 	}
 
 	for _, test := range tests {
 		test.receiver.Remove(test.input)
-		if len(test.receiver.words) != len(test.expected.words) {
-			t.Fatalf("(*IntSet).Remove(int) failed: expected is %v but actual is %v", test.expected, test.receiver)
+		if test.receiver.Len() != len(test.expected) {
+			t.Fatalf("(*ex02.IntSet).Len() in (*ex02.IntSet).Remove(int) failed: expected is %d but actual is %d", len(test.expected), test.receiver.Len())
 		}
-		for i := range test.receiver.words {
-			if test.receiver.words[i] != test.expected.words[i] {
-				t.Fatalf("(*IntSet).Remove(int) failed: expected is %v but actual is %v", test.expected, test.receiver)
+		for _, integer := range test.expected {
+			if !test.receiver.Has(integer) {
+				t.Fatalf("(*ex02.IntSet).Has(int) in *ex02.IntSet).Remove(int) failed: receiver is expected to have %d", integer)
 			}
 		}
 	}
@@ -97,24 +108,25 @@ func TestIntSet_Remove(t *testing.T) {
 
 func TestIntSet_Clear(t *testing.T) {
 	tests := []struct {
-		receiver IntSet
-		expected IntSet
+		receiver *ex02.IntSet
+		values   []int
+		expected []int
 	}{
-		{IntSet{[]uint64{0}}, IntSet{[]uint64{0}}},
-		{IntSet{[]uint64{2}}, IntSet{[]uint64{0}}},
-		{IntSet{[]uint64{3}}, IntSet{[]uint64{0}}},
-		{IntSet{[]uint64{0, 1}}, IntSet{[]uint64{0, 0}}},
-		{IntSet{[]uint64{1, 1, 1, 1}}, IntSet{[]uint64{0, 0, 0, 0}}},
+		{ex02.NewIntSet(), []int{}, []int{}},
+		{ex02.NewIntSet(1), []int{1}, []int{}},
+		{ex02.NewIntSet(0, 1), []int{0, 1}, []int{}},
+		{ex02.NewIntSet(64), []int{64}, []int{}},
+		{ex02.NewIntSet(0, 64, 128, 192), []int{0, 64, 128, 192}, []int{}},
 	}
 
 	for _, test := range tests {
 		test.receiver.Clear()
-		if len(test.receiver.words) != len(test.expected.words) {
-			t.Fatalf("(*IntSet).Clear() failed: expected is %v but actual is %v", test.expected, test.receiver)
+		if test.receiver.Len() != 0 {
+			t.Fatalf("(*ex02.IntSet).Clear() failed: receiver is expected to be length 0 but length %d", test.receiver.Len())
 		}
-		for i := range test.receiver.words {
-			if test.receiver.words[i] != test.expected.words[i] {
-				t.Fatalf("(*IntSet).Clear() failed: expected is %v but actual is %v", test.expected, test.receiver)
+		for _, value := range test.values {
+			if test.receiver.Has(value) {
+				t.Fatalf("(*ex02.IntSet).Clear() failed: receiver is expected to have no valeu but has %d", value)
 			}
 		}
 	}
@@ -122,24 +134,24 @@ func TestIntSet_Clear(t *testing.T) {
 
 func TestIntSet_Copy(t *testing.T) {
 	tests := []struct {
-		receiver IntSet
-		expected IntSet
+		receiver *ex02.IntSet
+		expected []int
 	}{
-		{IntSet{[]uint64{0}}, IntSet{[]uint64{0}}},
-		{IntSet{[]uint64{2}}, IntSet{[]uint64{2}}},
-		{IntSet{[]uint64{3}}, IntSet{[]uint64{3}}},
-		{IntSet{[]uint64{0, 1}}, IntSet{[]uint64{0, 1}}},
-		{IntSet{[]uint64{1, 1, 1, 1}}, IntSet{[]uint64{1, 1, 1, 1}}},
+		{ex02.NewIntSet(), []int{}},
+		{ex02.NewIntSet(1), []int{1}},
+		{ex02.NewIntSet(0, 1), []int{0, 1}},
+		{ex02.NewIntSet(64), []int{64}},
+		{ex02.NewIntSet(0, 64, 128, 192), []int{0, 64, 128, 192}},
 	}
 
 	for _, test := range tests {
 		actual := test.receiver.Copy()
-		if len(actual.words) != len(test.expected.words) {
-			t.Fatalf("(*IntSet).Copy() failed: expected is %v but actual is %v", test.expected, actual)
+		if actual.Len() != len(test.expected) {
+			t.Fatalf("(*ex02.IntSet).Len() in (*ex02.IntSet).Copy() failed: expected is %d but actual is %d", len(test.expected), actual.Len())
 		}
-		for i := range actual.words {
-			if actual.words[i] != test.expected.words[i] {
-				t.Fatalf("(*IntSet).Copy() failed: expected is %v but actual is %v", test.expected, actual)
+		for _, integer := range test.expected {
+			if !actual.Has(integer) {
+				t.Fatalf("(*ex02.IntSet).Has() in (*ex02.IntSet).Copy() failed: actual is expected to have %d", integer)
 			}
 		}
 	}
