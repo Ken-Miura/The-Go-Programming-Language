abe4c281efd757b9a8c4ce279f517c0d7cdb2173
Ken-Miura <ken.miura1102@gmail.com>
2017-06-11 11:45:54 +0900

    次の問題で利用できるように可視性修正+パッケージ分離

--- ch07/ex08/main.go
+++ ch07/ex08/main.go
@@ -7,17 +7,11 @@ import (
 	"sort"
 	"text/tabwriter"
 	"time"
+
+	"github.com/Ken-Miura/The-Go-Programming-Language/ch07/ex08/track_sort"
 )
 
-type Track struct {
-	Title  string
-	Artist string
-	Album  string
-	Year   int
-	Length time.Duration
-}
-
-var tracks = []*Track{
+var tracks = []*track_sort.Track{
 	{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
 	{"Go", "Moby", "Moby", 1992, length("3m37s")},
 	{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
@@ -32,7 +26,7 @@ func length(s string) time.Duration {
 	return d
 }
 
-func printTracks(tracks []*Track) {
+func printTracks(tracks []*track_sort.Track) {
 	const format = "%v\t%v\t%v\t%v\t%v\t\n"
 	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
 	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
@@ -43,74 +37,18 @@ func printTracks(tracks []*Track) {
 	tw.Flush() // calculate column widths and print table
 }
 
-type byTitle []*Track
-
-func (x byTitle) Len() int           { return len(x) }
-func (x byTitle) Less(i, j int) bool { return x[i].Title < x[j].Title }
-func (x byTitle) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
-
-type byYear []*Track
-
-func (x byYear) Len() int           { return len(x) }
-func (x byYear) Less(i, j int) bool { return x[i].Year < x[j].Year }
-func (x byYear) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
-
-type byLength []*Track
-
-func (x byLength) Len() int           { return len(x) }
-func (x byLength) Less(i, j int) bool { return x[i].Length < x[j].Length }
-func (x byLength) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
-
-type CustomSort struct {
-	SortKeys []string
-	t        []*Track
-}
-
-func (x CustomSort) Len() int           { return len(x.t) }
-func (x CustomSort) Less(i, j int) bool { return x.less(x.t[i], x.t[j]) }
-func (x CustomSort) less(a, b *Track) bool {
-	for i := len(x.SortKeys) - 1; i > -1; i-- {
-		switch x.SortKeys[i] {
-		case "Title":
-			if a.Title != b.Title {
-				return a.Title < b.Title
-			}
-		case "Artist":
-			if a.Artist != b.Artist {
-				return a.Artist < b.Artist
-			}
-		case "Album":
-			if a.Album != b.Album {
-				return a.Album < b.Album
-			}
-		case "Year":
-			if a.Year != b.Year {
-				return a.Year < b.Year
-			}
-		case "Length":
-			if a.Length != b.Length {
-				return a.Length < b.Length
-			}
-		default:
-			panic("This line must not be reached.")
-		}
-	}
-	return false
-}
-func (x CustomSort) Swap(i, j int) { x.t[i], x.t[j] = x.t[j], x.t[i] }
-
 func main() {
 	fmt.Println("Custom:")
 	fmt.Println("Primary: Title, Secondary: Year, Tertiary: Length")
-	sort.Sort(CustomSort{[]string{"Length", "Year", "Title"}, tracks})
+	sort.Sort(track_sort.CustomSort{[]string{"Length", "Year", "Title"}, tracks})
 	printTracks(tracks)
 
 	// sort.Stableは、一つ前のソート結果を保ちつつ、新しく与えられたキーでソートするように見える。
 	// 下記の出力は、一次キー: Year, 二次キー: Lengthの結果となった。
 	fmt.Println("\nStable Sort:")
 	fmt.Println("sort.Stable sort by Title at first, next by Year and finally by Length")
-	sort.Stable(byTitle(tracks))
-	sort.Stable(byYear(tracks))
-	sort.Stable(byLength(tracks))
+	sort.Stable(track_sort.ByTitle(tracks))
+	sort.Stable(track_sort.ByYear(tracks))
+	sort.Stable(track_sort.ByLength(tracks))
 	printTracks(tracks)
 }
---
+++ ch07/ex08/track_sort/track_sort.go
@@ -0,0 +1,68 @@
+// Copyright 2017 Ken Miura
+package track_sort
+
+import "time"
+
+type Track struct {
+	Title  string
+	Artist string
+	Album  string
+	Year   int
+	Length time.Duration
+}
+
+type ByTitle []*Track
+
+func (x ByTitle) Len() int           { return len(x) }
+func (x ByTitle) Less(i, j int) bool { return x[i].Title < x[j].Title }
+func (x ByTitle) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+type ByYear []*Track
+
+func (x ByYear) Len() int           { return len(x) }
+func (x ByYear) Less(i, j int) bool { return x[i].Year < x[j].Year }
+func (x ByYear) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+type ByLength []*Track
+
+func (x ByLength) Len() int           { return len(x) }
+func (x ByLength) Less(i, j int) bool { return x[i].Length < x[j].Length }
+func (x ByLength) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+type CustomSort struct {
+	SortKeys []string
+	T        []*Track
+}
+
+func (x CustomSort) Len() int           { return len(x.T) }
+func (x CustomSort) Less(i, j int) bool { return x.less(x.T[i], x.T[j]) }
+func (x CustomSort) less(a, b *Track) bool {
+	for i := len(x.SortKeys) - 1; i > -1; i-- {
+		switch x.SortKeys[i] {
+		case "Title":
+			if a.Title != b.Title {
+				return a.Title < b.Title
+			}
+		case "Artist":
+			if a.Artist != b.Artist {
+				return a.Artist < b.Artist
+			}
+		case "Album":
+			if a.Album != b.Album {
+				return a.Album < b.Album
+			}
+		case "Year":
+			if a.Year != b.Year {
+				return a.Year < b.Year
+			}
+		case "Length":
+			if a.Length != b.Length {
+				return a.Length < b.Length
+			}
+		default:
+			panic("This line must not be reached.")
+		}
+	}
+	return false
+}
+func (x CustomSort) Swap(i, j int) { x.T[i], x.T[j] = x.T[j], x.T[i] }
