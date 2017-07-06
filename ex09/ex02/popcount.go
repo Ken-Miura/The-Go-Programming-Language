6067e695f566bac1642995c59953825b571474a5
Ken-Miura <ken.miura1102@gmail.com>
2017-07-07 00:31:19 +0900

    練習問題追加

---
+++ ex09/ex02/popcount.go
@@ -0,0 +1,28 @@
+// Copyright 2017 Ken Miura
+package ex02
+
+import "sync"
+
+var initializeOnce sync.Once
+
+// pc[i] is the population count of i.
+var pc [256]byte
+
+func initializeLookupTable() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCount returns the population count (number of set bits) of x.
+func PopCount(x uint64) int {
+	initializeOnce.Do(initializeLookupTable)
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
---
+++ ex09/ex02/popcount_test.go
@@ -0,0 +1,27 @@
+// Copyright 2017 Ken Miura
+package ex02_test
+
+import (
+	"testing"
+
+	"github.com/Ken-Miura/The-Go-Programming-Language/ex09/ex02"
+	"gopl.io/ch2/popcount"
+)
+
+func TestPopCountUsingLoop(t *testing.T) { // テキストのサンプルコードの関数Popcountと同じ出力をするか確認
+	var popcountTests = []struct {
+		n        uint64 // input
+		expected int    // expected
+	}{
+		{0x1234567890ABCDEF, popcount.PopCount(0x1234567890ABCDEF)},
+		{0, popcount.PopCount(0)},
+		{1234567890987654321, popcount.PopCount(1234567890987654321)},
+	}
+
+	for _, tt := range popcountTests {
+		actual := ex02.PopCount(tt.n)
+		if actual != tt.expected {
+			t.Fatalf("expected is %d but actual is %d", tt.expected, actual)
+		}
+	}
+}
