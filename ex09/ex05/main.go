48a5479c72db5f1b1587fb91fea5954ade65b94c
Ken-Miura <ken.miura1102@gmail.com>
2017-07-08 15:22:40 +0900

    練習問題追加

--- ex09/ex05/main.go
+++ ex09/ex05/main.go
@@ -3,34 +3,62 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"sync"
+	"time"
 )
 
 func main() {
 
 	ch1 := make(chan string)
 	ch2 := make(chan string)
+	abort := make(chan string)
+	var wg sync.WaitGroup
+	wg.Add(1)
 	go func() {
-		ch1 <- "hello"
+		defer wg.Done()
+		times := 0
 		for {
-			message := <-ch2
-			ch1 <- message
-			fmt.Println("p1")
+			select {
+			case message := <-ch2:
+				ch1 <- message
+				times++
+			case <-abort:
+				close(ch1)
+				for range ch2 {
+
+				}
+				fmt.Printf("first goroutine has received and sent %d times for 1s\n", times)
+				return
+			}
 		}
 	}()
 
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		for {
-			message := <-ch1
-			ch2 <- message
-			fmt.Println("p2")
+			times := 0
+			for {
+				select {
+				case message := <-ch1:
+					ch2 <- message
+					times++
+				case <-abort:
+					close(ch2)
+					for range ch1 {
+
+					}
+					fmt.Printf("second goroutine has received and sent %d times for 1s\n", times)
+					return
+				}
+			}
 		}
 	}()
 
-	done := make(chan struct{})
-	go func() {
-		os.Stdin.Read(make([]byte, 1))
-		close(done)
-	}()
-	<-done
+	ch1 <- "hello"
+	select {
+	case <-time.After(1 * time.Second):
+		close(abort)
+	}
+	wg.Wait()
 }
