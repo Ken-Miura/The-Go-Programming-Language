533ae17f86229b971e4f706247aaed65967d5ba3
Ken-Miura <ken.miura1102@gmail.com>
2017-07-16 14:44:32 +0900

    改行処理＋TODO削除

--- ch07/ex02/counter_test.go
+++ ch07/ex02/counter_test.go
@@ -7,16 +7,13 @@ import (
 )
 
 var tests = []struct {
-	input1    string
-	expected1 int64
-	input2    string
-	expected2 int64
-	input3    string
-	expected3 int64
-	input4    string
-	expected4 int64
+	input    string
+	expected int64
 }{
-	{"", 0, "hello", 5, " ", 6, "world", 11},
+	{"", 0},
+	{"hello", 5},
+	{" ", 1},
+	{"world", 5},
 }
 
 func TestCountingWriter(t *testing.T) {
@@ -24,21 +21,9 @@ func TestCountingWriter(t *testing.T) {
 	for _, test := range tests {
 		var buf bytes.Buffer
 		writer, pCounter := CountingWriter(&buf)
-		_, _ = writer.Write([]byte(test.input1))
-		if *pCounter != test.expected1 {
-			t.Fatalf("%d is expected but result1 is %d", test.expected1, *pCounter)
-		}
-		_, _ = writer.Write([]byte(test.input2))
-		if *pCounter != test.expected2 {
-			t.Fatalf("%d is expected but result1 is %d", test.expected2, *pCounter)
-		}
-		_, _ = writer.Write([]byte(test.input3))
-		if *pCounter != test.expected3 {
-			t.Fatalf("%d is expected but result1 is %d", test.expected3, *pCounter)
-		}
-		_, _ = writer.Write([]byte(test.input4))
-		if *pCounter != test.expected4 {
-			t.Fatalf("%d is expected but result1 is %d", test.expected4, *pCounter)
+		_, _ = writer.Write([]byte(test.input))
+		if *pCounter != test.expected {
+			t.Fatalf("%d is expected but result1 is %d", test.expected, *pCounter)
 		}
 	}
 }
