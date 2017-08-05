26c78a9ca9bbd99918323e14e8c213b41ed6fe16
Ken-Miura <ken.miura1102@gmail.com>
2017-08-05 22:29:47 +0900

    ファイル展開の実装

--- ch10/ex02/main.go
+++ ch10/ex02/main.go
@@ -2,7 +2,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/Ken-Miura/The-Go-Programming-Language/ch10/ex02/archive"
@@ -11,15 +13,39 @@ import (
 )
 
 func main() {
-	test := "tar_test.tar"
-	// test = "zip_test.zip"
-	items, method, err := archive.Extract(test)
+	if len(os.Args) != 2 {
+		fmt.Println("usage: " + os.Args[0] + " 'archive file you want to extract'")
+		fmt.Println("ex: " + os.Args[0] + " tar_test.tar")
+		os.Exit(0)
+	}
+	items, method, err := archive.Extract(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Println(method)
+	fmt.Println("archive method: " + method)
 	for _, item := range items {
-		fmt.Println(item.Info.Name())
+		err = createFile(item.Info.Name(), item.Contents)
+		if err != nil {
+			fmt.Printf("failed to create file %s: %v\n", item.Info.Name(), err)
+		}
 	}
+}
+
+func createFile(fileName string, contents []byte) (err error) {
+	f, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if closeErr := f.Close(); err == nil {
+			err = closeErr
+		}
+	}()
+	buf := bytes.NewBuffer(contents)
+	_, err = io.Copy(f, buf)
+	if err != nil {
+		return err
+	}
+	return nil
 }
---
+++ ch10/ex02/runtall.bat
@@ -0,0 +1,2 @@
+go run main.go tar_test.tar
+go run main.go zip_test.zip
