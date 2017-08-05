d4b356c3af06a79ff2310597dabbdb184db5bf13
Ken-Miura <ken.miura1102@gmail.com>
2017-08-06 06:11:48 +0900

    バッチ追加

--- ch10/ex04/main.go
+++ ch10/ex04/main.go
@@ -56,7 +56,7 @@ func findImportPathByPackageName(pkg string) (string, error) {
 
 func findDependencies(importPath string) ([]string, error) {
 	cmd := exec.Command("go", "list", "-f",
-		`{ "Name":"{{.Name}}", "Deps":["{{ join .Deps "\", \"" }}"]},`, "...")
+		`{ "Name":"{{.Name}}", "Deps":["{{ join .Deps "\", \"" }}"]},`, "...") // ...1
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect with %s's stdout: %v", cmd.Args[0], err)
@@ -69,20 +69,22 @@ func findDependencies(importPath string) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
+	if err := cmd.Wait(); err != nil {
+		return nil, fmt.Errorf("failed to wait for command (%s): %v", cmd.Args[0], err)
+	}
+
+	// 1のフォーマットによる出力が正しいJSON配列でないので微修正 (最後にカンマ有りかつ[と]でくくられていない)
 	tmp := buf.String()
 	i := strings.LastIndex(tmp, ",")
-	tmp = "[" + tmp[:i] + "]"
+	jsonArray := fmt.Sprintf("[%s]", tmp[:i])
 
 	type packageInfo struct {
 		Name string
 		Deps []string
 	}
 	var allPackages []packageInfo
-	if err := json.NewDecoder(strings.NewReader(tmp)).Decode(&allPackages); err != nil {
+	if err := json.NewDecoder(strings.NewReader(jsonArray)).Decode(&allPackages); err != nil {
 		return nil, fmt.Errorf("failed to decode as json: %v", err)
-	}
-	if err := cmd.Wait(); err != nil {
-		return nil, fmt.Errorf("failed to wait for command (%s): %v", cmd.Args[0], err)
 	}
 	var dependencies []string
 loop:
