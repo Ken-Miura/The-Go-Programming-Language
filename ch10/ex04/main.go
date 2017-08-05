04ebdd71b63647f64472248461930450880e6927
Ken-Miura <ken.miura1102@gmail.com>
2017-08-06 06:30:39 +0900

    一部内容を別メソッドに抽出

--- ch10/ex04/main.go
+++ ch10/ex04/main.go
@@ -55,40 +55,21 @@ func findImportPathByPackageName(pkg string) (string, error) {
 }
 
 func findDependencies(importPath string) ([]string, error) {
-	cmd := exec.Command("go", "list", "-f",
-		`{ "Name":"{{.Name}}", "Deps":["{{ join .Deps "\", \"" }}"]},`, "...") // ...1
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return nil, fmt.Errorf("failed to connect with %s's stdout: %v", cmd.Args[0], err)
-	}
-	if err := cmd.Start(); err != nil {
-		return nil, fmt.Errorf("failed to start command (%s): %v", cmd.Args[0], err)
-	}
-	buf := new(bytes.Buffer)
-	_, err = io.Copy(buf, stdout)
+	pkgsString, err := getAllPackageInfoAsJson()
 	if err != nil {
 		return nil, err
 	}
-	if err := cmd.Wait(); err != nil {
-		return nil, fmt.Errorf("failed to wait for command (%s): %v", cmd.Args[0], err)
-	}
-
-	// 1のフォーマットによる出力が正しいJSON配列でないので微修正 (最後にカンマ有りかつ[と]でくくられていない)
-	tmp := buf.String()
-	i := strings.LastIndex(tmp, ",")
-	jsonArray := fmt.Sprintf("[%s]", tmp[:i])
-
 	type packageInfo struct {
 		Name string
 		Deps []string
 	}
-	var allPackages []packageInfo
-	if err := json.NewDecoder(strings.NewReader(jsonArray)).Decode(&allPackages); err != nil {
+	var allPkgs []packageInfo
+	if err := json.NewDecoder(strings.NewReader(pkgsString)).Decode(&allPkgs); err != nil {
 		return nil, fmt.Errorf("failed to decode as json: %v", err)
 	}
 	var dependencies []string
 loop:
-	for _, pkg := range allPackages {
+	for _, pkg := range allPkgs {
 		for _, dep := range pkg.Deps {
 			if dep == importPath {
 				dependencies = append(dependencies, pkg.Name)
@@ -97,4 +78,29 @@ loop:
 		}
 	}
 	return dependencies, nil
+}
+
+func getAllPackageInfoAsJson() (string, error) {
+	cmd := exec.Command("go", "list", "-f",
+		`{ "Name":"{{.Name}}", "Deps":["{{ join .Deps "\", \"" }}"]},`, "...") // ...1
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return "", fmt.Errorf("failed to connect with %s's stdout: %v", cmd.Args[0], err)
+	}
+	if err := cmd.Start(); err != nil {
+		return "", fmt.Errorf("failed to start command (%s): %v", cmd.Args[0], err)
+	}
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, stdout)
+	if err != nil {
+		return "", err
+	}
+	if err := cmd.Wait(); err != nil {
+		return "", fmt.Errorf("failed to wait for command (%s): %v", cmd.Args[0], err)
+	}
+
+	// 1のフォーマットによる出力が正しいJSON配列でないので微修正 (最後にカンマ有りかつ[と]でくくられていない)
+	tmp := buf.String()
+	i := strings.LastIndex(tmp, ",")
+	return fmt.Sprintf("[%s]", tmp[:i]), nil
 }
