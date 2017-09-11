5956a6b1b4d22794b671da573282d23d6cb16095
Ken-Miura <ken.miura1102@gmail.com>
2017-09-12 00:59:44 +0900

    型と変数をパッケージで共有するように変更

--- ch12/ex03/sexpr_test.go
+++ ch12/ex03/sexpr_test.go
@@ -22,32 +22,6 @@ func TestComplex(t *testing.T) {
 }
 
 func TestInterface(t *testing.T) {
-	type Movie struct {
-		Title, Subtitle string
-		Year            int
-		Actor           map[string]string
-		Oscars          []string
-		Sequel          *string
-	}
-	strangelove := Movie{
-		Title:    "Dr. Strangelove",
-		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
-		Year:     1964,
-		Actor: map[string]string{
-			"Dr. Strangelove":            "Peter Sellers",
-			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
-			"Pres. Merkin Muffley":       "Peter Sellers",
-			"Gen. Buck Turgidson":        "George C. Scott",
-			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
-			`Maj. T.J. "King" Kong`:      "Slim Pickens",
-		},
-		Oscars: []string{
-			"Best Actor (Nomin.)",
-			"Best Adapted Screenplay (Nomin.)",
-			"Best Director (Nomin.)",
-			"Best Picture (Nomin.)",
-		},
-	}
 	var i interface{} = strangelove
 	// Encode it
 	data, err := Marshal(&i)
@@ -232,33 +206,6 @@ func testComplex128(t *testing.T) {
 // 	$ go test -v gopl.io/ch12/sexpr
 //
 func Test(t *testing.T) {
-	type Movie struct {
-		Title, Subtitle string
-		Year            int
-		Actor           map[string]string
-		Oscars          []string
-		Sequel          *string
-	}
-	strangelove := Movie{
-		Title:    "Dr. Strangelove",
-		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
-		Year:     1964,
-		Actor: map[string]string{
-			"Dr. Strangelove":            "Peter Sellers",
-			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
-			"Pres. Merkin Muffley":       "Peter Sellers",
-			"Gen. Buck Turgidson":        "George C. Scott",
-			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
-			`Maj. T.J. "King" Kong`:      "Slim Pickens",
-		},
-		Oscars: []string{
-			"Best Actor (Nomin.)",
-			"Best Adapted Screenplay (Nomin.)",
-			"Best Director (Nomin.)",
-			"Best Picture (Nomin.)",
-		},
-	}
-
 	// Encode it
 	data, err := Marshal(strangelove)
 	if err != nil {
@@ -284,4 +231,32 @@ func Test(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Logf("MarshalIdent() = %s\n", data)
+}
+
+type Movie struct {
+	Title, Subtitle string
+	Year            int
+	Actor           map[string]string
+	Oscars          []string
+	Sequel          *string
+}
+
+var strangelove = Movie{
+	Title:    "Dr. Strangelove",
+	Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+	Year:     1964,
+	Actor: map[string]string{
+		"Dr. Strangelove":            "Peter Sellers",
+		"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+		"Pres. Merkin Muffley":       "Peter Sellers",
+		"Gen. Buck Turgidson":        "George C. Scott",
+		"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+		`Maj. T.J. "King" Kong`:      "Slim Pickens",
+	},
+	Oscars: []string{
+		"Best Actor (Nomin.)",
+		"Best Adapted Screenplay (Nomin.)",
+		"Best Director (Nomin.)",
+		"Best Picture (Nomin.)",
+	},
 }
