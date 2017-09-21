289293966cecf6b273af03e0305e1bae53098fbd
Ken-Miura <ken.miura1102@gmail.com>
2017-09-22 01:32:05 +0900

    練習問題修正

--- ch12/ex07/decode.go
+++ ch12/ex07/decode.go
@@ -11,19 +11,25 @@ import (
 )
 
 type Decoder struct {
-	r io.Reader
+	lex *lexer
 }
 
 func NewDecoder(r io.Reader) *Decoder {
-	return &Decoder{r}
+	lex := &lexer{scan: scanner.Scanner{Mode: scanner.GoTokens}}
+	lex.scan.Init(r)
+	return &Decoder{lex: lex}
 }
 
-func (dec *Decoder) Decode(out interface{}) error {
-	var buf bytes.Buffer
-	if _, err := io.Copy(&buf, dec.r); err != nil {
-		return err
-	}
-	return Unmarshal(buf.Bytes(), out)
+func (dec *Decoder) Decode(out interface{}) (err error) {
+	dec.lex.next() // get the first token
+	defer func() {
+		// NOTE: this is not an example of ideal error handling.
+		if x := recover(); x != nil {
+			err = fmt.Errorf("error at %s: %v", dec.lex.scan.Position, x)
+		}
+	}()
+	read(dec.lex, reflect.ValueOf(out).Elem())
+	return nil
 }
 
 var TypeCache = make(map[string]reflect.Type)
