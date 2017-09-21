5a71b27894cf41288cac26b65c4ab54eb2bd605a
Ken-Miura <ken.miura1102@gmail.com>
2017-09-22 01:30:11 +0900

    練習問題追加

---
+++ ch12/ex08/decode.go
@@ -0,0 +1,179 @@
+// Copyright 2017 Ken Miura
+package ex08
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"reflect"
+	"strconv"
+	"text/scanner"
+)
+
+type Decoder struct {
+	lex *lexer
+}
+
+func NewDecoder(r io.Reader) *Decoder {
+	lex := &lexer{scan: scanner.Scanner{Mode: scanner.GoTokens}}
+	lex.scan.Init(r)
+	return &Decoder{lex: lex}
+}
+
+func (dec *Decoder) Decode(out interface{}) (err error) {
+	dec.lex.next() // get the first token
+	defer func() {
+		// NOTE: this is not an example of ideal error handling.
+		if x := recover(); x != nil {
+			err = fmt.Errorf("error at %s: %v", dec.lex.scan.Position, x)
+		}
+	}()
+	read(dec.lex, reflect.ValueOf(out).Elem())
+	return nil
+}
+
+var TypeCache = make(map[string]reflect.Type)
+
+type Movie struct {
+	Title, Subtitle string
+	Year            int
+	Actor           map[string]string
+	Oscars          []string
+	Sequel          *string
+}
+
+func init() {
+	TypeCache["ex08.Movie"] = reflect.TypeOf(Movie{})
+}
+
+func Unmarshal(data []byte, out interface{}) (err error) {
+	dec := NewDecoder(bytes.NewReader(data))
+	return dec.Decode(out)
+}
+
+type lexer struct {
+	scan  scanner.Scanner
+	token rune // the current token
+}
+
+func (lex *lexer) next()        { lex.token = lex.scan.Scan() }
+func (lex *lexer) text() string { return lex.scan.TokenText() }
+
+func (lex *lexer) consume(want rune) {
+	if lex.token != want { // NOTE: Not an example of good error handling.
+		panic(fmt.Sprintf("got %q, want %q", lex.text(), want))
+	}
+	lex.next()
+}
+
+func read(lex *lexer, v reflect.Value) {
+	switch lex.token {
+	case scanner.Ident:
+		symbol := lex.text()
+		if symbol == "nil" {
+			v.Set(reflect.Zero(v.Type()))
+			lex.next()
+			return
+		} else if symbol == "t" {
+			v.SetBool(true)
+			lex.next()
+			return
+		}
+	case scanner.String:
+		s, _ := strconv.Unquote(lex.text()) // NOTE: ignoring errors
+		v.SetString(s)
+		lex.next()
+		return
+	case scanner.Int:
+		i, _ := strconv.Atoi(lex.text()) // NOTE: ignoring errors
+		v.SetInt(int64(i))
+		lex.next()
+		return
+	case scanner.Float:
+		f, _ := strconv.ParseFloat(lex.text(), 64) // NOTE: ignoring errors
+		v.SetFloat(f)
+		lex.next()
+		return
+	case '#':
+		// #C(%g %g) format
+		lex.next()                                    // consume '#'
+		lex.next()                                    // consume 'C'
+		lex.next()                                    // consume '('
+		real, _ := strconv.ParseFloat(lex.text(), 64) // NOTE: ignoring errors
+		lex.next()                                    // consume ' '
+		imag, _ := strconv.ParseFloat(lex.text(), 64) // NOTE: ignoring errors
+		lex.next()                                    // consume ')'
+		v.SetComplex(complex(real, imag))
+		return
+	case '(':
+		lex.next()
+		readList(lex, v)
+		lex.next() // consume ')'
+		return
+	}
+	panic(fmt.Sprintf("unexpected token %q", lex.text()))
+}
+
+func readList(lex *lexer, v reflect.Value) {
+	switch v.Kind() {
+	case reflect.Array: // (item ...)
+		for i := 0; !endList(lex); i++ {
+			read(lex, v.Index(i))
+		}
+
+	case reflect.Slice: // (item ...)
+		for !endList(lex) {
+			item := reflect.New(v.Type().Elem()).Elem()
+			read(lex, item)
+			v.Set(reflect.Append(v, item))
+		}
+
+	case reflect.Struct: // ((name value) ...)
+		for !endList(lex) {
+			lex.consume('(')
+			if lex.token != scanner.Ident {
+				panic(fmt.Sprintf("got token %q, want field name", lex.text()))
+			}
+			name := lex.text()
+			lex.next()
+			read(lex, v.FieldByName(name))
+			lex.consume(')')
+		}
+
+	case reflect.Map: // ((key value) ...)
+		v.Set(reflect.MakeMap(v.Type()))
+		for !endList(lex) {
+			lex.consume('(')
+			key := reflect.New(v.Type().Key()).Elem()
+			read(lex, key)
+			value := reflect.New(v.Type().Elem()).Elem()
+			read(lex, value)
+			v.SetMapIndex(key, value)
+			lex.consume(')')
+		}
+
+	case reflect.Interface:
+		typeName, _ := strconv.Unquote(lex.text()) // NOTE: ignoring errors
+		typ, ok := TypeCache[typeName]
+		if !ok {
+			panic(fmt.Sprintf("unsupported type %q", typeName))
+		}
+		lex.next() // consume "type name"
+		rv := reflect.New(typ).Elem()
+		read(lex, rv)
+		v.Set(rv)
+
+	default:
+		panic(fmt.Sprintf("cannot decode list into %v", v.Type()))
+	}
+}
+
+func endList(lex *lexer) bool {
+	switch lex.token {
+	case scanner.EOF:
+		panic("end of file")
+	case ')':
+		return true
+	}
+	return false
+}
---
+++ ch12/ex08/sexpr_test.go
@@ -0,0 +1,195 @@
+// Copyright 2017 Ken Miura
+package ex08
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBool(t *testing.T) {
+	test(true, t)
+	test(false, t)
+}
+
+func TestFloat(t *testing.T) {
+	testFloat32(t)
+	testFloat64(t)
+}
+
+func TestComplex(t *testing.T) {
+	testComplex64(t)
+	testComplex128(t)
+}
+
+func TestInterface(t *testing.T) {
+	var i interface{} = strangelove
+	// Encode it
+	data, err := Marshal(&i)
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	t.Logf("Marshal() = %s\n", data)
+
+	// Decode it
+	var decodedI interface{}
+	if err := Unmarshal(data, &decodedI); err != nil {
+		t.Fatalf("Unmarshal failed: %v", err)
+	}
+	t.Logf("Unmarshal() = %+v\n", decodedI)
+
+	// Check equality.
+	if !reflect.DeepEqual(decodedI, strangelove) {
+		t.Fatal("not equal")
+	}
+}
+
+func test(b bool, t *testing.T) {
+	// Encode it
+	data, err := Marshal(b)
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	t.Logf("Marshal() = %s\n", data)
+
+	// Decode it
+	var decodedB bool
+	if err := Unmarshal(data, &decodedB); err != nil {
+		t.Fatalf("Unmarshal failed: %v", err)
+	}
+	t.Logf("Unmarshal() = %+v\n", decodedB)
+
+	// Check equality.
+	if decodedB != b {
+		t.Fatal("not equal")
+	}
+}
+
+func testFloat32(t *testing.T) {
+	var f float32 = 3.75
+	// Encode it
+	data, err := Marshal(f)
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	t.Logf("Marshal() = %s\n", data)
+
+	// Decode it
+	var decodedF float32
+	if err := Unmarshal(data, &decodedF); err != nil {
+		t.Fatalf("Unmarshal failed: %v", err)
+	}
+	t.Logf("Unmarshal() = %+v\n", decodedF)
+
+	// Check equality.
+	//if !reflect.DeepEqual(decodedF, f) {
+	if decodedF != f {
+		t.Fatal("not equal")
+	}
+}
+
+func testFloat64(t *testing.T) {
+	var f float64 = 12345.67890
+	// Encode it
+	data, err := Marshal(f)
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	t.Logf("Marshal() = %s\n", data)
+
+	// Decode it
+	var decodedF float64
+	if err := Unmarshal(data, &decodedF); err != nil {
+		t.Fatalf("Unmarshal failed: %v", err)
+	}
+	t.Logf("Unmarshal() = %+v\n", decodedF)
+
+	// Check equality.
+	if decodedF != f {
+		t.Fatal("not equal")
+	}
+}
+
+func testComplex64(t *testing.T) {
+	var c complex64 = 1.25 + 0.75i
+	// Encode it
+	data, err := Marshal(c)
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	t.Logf("Marshal() = %s\n", data)
+
+	// Decode it
+	var decodedC complex64
+	if err := Unmarshal(data, &decodedC); err != nil {
+		t.Fatalf("Unmarshal failed: %v", err)
+	}
+	t.Logf("Unmarshal() = %+v\n", decodedC)
+
+	// Check equality.
+	if decodedC != c {
+		t.Fatal("not equal")
+	}
+}
+
+func testComplex128(t *testing.T) {
+	var c complex128 = 12345.67890 + 9876.543210i
+	// Encode it
+	data, err := Marshal(c)
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	t.Logf("Marshal() = %s\n", data)
+
+	// Decode it
+	var decodedC complex128
+	if err := Unmarshal(data, &decodedC); err != nil {
+		t.Fatalf("Unmarshal failed: %v", err)
+	}
+	t.Logf("Unmarshal() = %+v\n", decodedC)
+
+	// Check equality.
+	if decodedC != c {
+		t.Fatal("not equal")
+	}
+}
+
+func Test(t *testing.T) {
+	// Encode it
+	data, err := Marshal(strangelove)
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	t.Logf("Marshal() = %s\n", data)
+
+	// Decode it
+	var movie Movie
+	if err := Unmarshal(data, &movie); err != nil {
+		t.Fatalf("Unmarshal failed: %v", err)
+	}
+	t.Logf("Unmarshal() = %+v\n", movie)
+
+	// Check equality.
+	if !reflect.DeepEqual(movie, strangelove) {
+		t.Fatal("not equal")
+	}
+}
+
+var strangelove = Movie{
+	Title:    "Dr. Strangelove",
+	Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+	Year:     1964,
+	Actor: map[string]string{
+		"Dr. Strangelove":            "Peter Sellers",
+		"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+		"Pres. Merkin Muffley":       "Peter Sellers",
+		"Gen. Buck Turgidson":        "George C. Scott",
+		"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+		`Maj. T.J. "King" Kong`:      "Slim Pickens",
+	},
+	Oscars: []string{
+		"Best Actor (Nomin.)",
+		"Best Adapted Screenplay (Nomin.)",
+		"Best Director (Nomin.)",
+		"Best Picture (Nomin.)",
+	},
+}
