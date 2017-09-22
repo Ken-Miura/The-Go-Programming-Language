4e4d67d54de1ddb4e73973bb6c33cfd92227b71d
Ken-Miura <ken.miura1102@gmail.com>
2017-09-22 23:09:31 +0900

    リファクタリング

--- ch12/ex11/pack.go
+++ ch12/ex11/pack.go
@@ -57,47 +57,19 @@ func checkIfAllValuesAreDefault(values []reflect.Value) bool {
 func checkIfValueIsDefault(v reflect.Value) bool {
 	switch v.Kind() {
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		if v.Int() == 0 {
-			return true
-		} else {
-			return false
-		}
+		return v.Int() == 0
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		if v.Uint() == 0 {
-			return true
-		} else {
-			return false
-		}
+		return v.Uint() == 0
 	case reflect.String:
-		if v.String() == "" {
-			return true
-		} else {
-			return false
-		}
+		return v.String() == ""
 	case reflect.Bool:
-		if v.Bool() {
-			return false
-		} else {
-			return true
-		}
+		return !v.Bool()
 	case reflect.Float32, reflect.Float64:
-		if v.Float() == 0.0 {
-			return true
-		} else {
-			return false
-		}
+		return v.Float() == 0.0
 	case reflect.Complex64, reflect.Complex128:
-		if v.Complex() == 0+0i {
-			return true
-		} else {
-			return false
-		}
+		return v.Complex() == 0+0i
 	case reflect.Ptr, reflect.Slice, reflect.Map, reflect.Interface, reflect.Chan, reflect.Func:
-		if v.IsNil() {
-			return true
-		} else {
-			return false
-		}
+		return v.IsNil()
 	default:
 		return false
 	}
